14292fe5c32f27d6f2b65396c28f970f28ae4f4d
yuseonyoung <crownsy@naver.com>
2024-12-14 03:16:21 +0900

    create logo
    create logo

--- Modu_expense_book/modu_expend_book/src/components/Header.tsx
+++ Modu_expense_book/modu_expend_book/src/components/Header.tsx
@@ -1,25 +1,25 @@
 'use client'
 
-/* import Image from 'next/image' */
+import Image from 'next/image' 
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center mb-8">
       <div className="flex items-center gap-2">
-        {/* <Image 
-          src="/logo.png" 
+         <Image 
+          src="../icon/logo.png" 
           alt="Logo" 
           width={32} 
           height={32}
           className="h-8"
-        /> */}
+        /> 
 
-        <h1 className="text-xl font-bold text-white">Household Ledger</h1>
+        <h1 className="text-xl font-bold text-white">모두의 가계부</h1>
       </div>
       <nav className="flex gap-4">
-        <a href="#" className="text-white hover:text-gray-300">Dashboard</a>
-        <a href="#" className="text-white hover:text-gray-300">Reports</a>
-        <a href="#" className="text-white hover:text-gray-300">Settings</a>
+        <a href="#" className="text-white hover:text-gray-300">Home</a>
+        <a href="#" className="text-white hover:text-gray-300">보고서</a>
+        <a href="#" className="text-white hover:text-gray-300">마이페이지</a>
       </nav>
     </header>
   )
