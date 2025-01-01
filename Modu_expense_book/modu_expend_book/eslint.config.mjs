16511e43a1d93547403138e9c6298b1198495d84
yuseonyoung <crownsy@naver.com>
2025-01-01 16:59:27 +0900

    eslint 설정 off
    eslint 설정 off

--- Modu_expense_book/modu_expend_book/eslint.config.mjs
+++ Modu_expense_book/modu_expend_book/eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/no-unescaped-entities": "off"
+    },
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    }
+  }
 ];
 
 export default eslintConfig;
