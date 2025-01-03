2e7d85bab7283e18ffa53783eabf70417f55e8b5
yuseonyoung <crownsy@naver.com>
2025-01-03 15:46:20 +0900

    update layout
    update layout

--- Modu_expense_book/modu_expend_book/src/app/layout.tsx
+++ Modu_expense_book/modu_expend_book/src/app/layout.tsx
@@ -1,41 +1,46 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Noto_Sans_KR } from 'next/font/google'
 import '@/app/globals.css'
 import LayoutClient from '@/app/layoutClient'
 import { Toaster } from "@/components/ui/toaster"
 
 const notoSansKR = Noto_Sans_KR({ 
-  subsets: ['latin'],
-  weight: ['400', '500', '700']
+ subsets: ['latin'],
+ weight: ['400', '500', '700']
 })
 
 export const metadata: Metadata = {
-  title: '모두의 가계부',
-  description: '개인 예산 관리 대시보드',
-  manifest: '/manifest.json',
-  themeColor: '#ffffff',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+ title: '모두의 가계부',
+ description: '개인 예산 관리 대시보드',
+ manifest: '/manifest.json',
+}
+
+export const viewport: Viewport = {
+ width: 'device-width',
+ initialScale: 1,
+ maximumScale: 1,
+ viewportFit: 'cover',
+ themeColor: '#ffffff',
 }
 
 export default function RootLayout({
-  children,
+ children,
 }: {
-  children: React.ReactNode
+ children: React.ReactNode
 }) {
-  return (
-    <html lang="ko">
-      <head>
-        <link
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          rel="stylesheet"
-        />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-      </head>
-      <body className={notoSansKR.className}>
-        <LayoutClient>{children}</LayoutClient>
-        <Toaster />
-
-      </body>
-    </html>
-  )
+ return (
+   <html lang="ko">
+     <head>
+       <link
+         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+         rel="stylesheet"
+       />
+       <link rel="apple-touch-icon" href="/icon-192x192.png" />
+     </head>
+     <body className={notoSansKR.className}>
+       <LayoutClient>{children}</LayoutClient>
+       <Toaster />
+     </body>
+   </html>
+ )
 }
